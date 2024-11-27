aac0edcd0d91d36b0c98434c7dc12df6edf1eb32
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2024-11-27 01:45:04 -0500

    :hammer: add file

--- Proyecto-Funciones.sql
+++ Proyecto-Funciones.sql
@@ -1,2 +1,21 @@
 use proyecto;
 
+-- 1. Calcula el total de una venta
+
+-- 2. Obtener el precio mas reciente de algun producto
+
+-- 3. Calcula el salario promedio de los empleados
+
+-- 4. Calcula el total de los productos vendidos
+
+-- 5. Muestra el estado en el que se encuentre cualquier maquina
+
+-- 6. Calcula si un producto tiene suficiente de ese mismo producto en stock
+
+-- 7. Muestra el nombre completo de un empleado
+
+-- 8. Muestra la cantidad total que se encuentran en el inventario
+
+-- 9. Calcula la cantidad total producida de un producto en proceso de produccion
+
+-- 10. Muestra el nombre completo de un cliente por su ID
---
+++ proyecto-Eventos.sql
@@ -0,0 +1,21 @@
+use proyecto;
+
+-- 1. Elimina del inventario los productos que sean obsoletas cada mes
+
+-- 2. Actualiza el estado de las maquinas cada semana
+
+-- 3. Actualizar el precio promedio de productos caada semana
+
+-- 4. Muestra a los empleados inactivos que no han producido nada en 6 meses
+
+-- 5. Genera un resumen de las ventas diarias
+
+-- 6. Muestra los productos mas vendidos semanalmente
+
+-- 7. Registra la maquinaria en proceso mensual de revision
+
+-- 8. Muestra las maquinas que se demoran mas de 6 meses en ser reparadas
+
+-- 9. Genera una notificacion cada vez que un producto en stock este bajo
+
+-- 10. Genera un informe mensual de la maquinaria mas usada
